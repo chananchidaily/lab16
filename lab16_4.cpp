05a54268df1bd882ab431c4b581fd5d64e682696
TK <chananchida_tha@cmu.ac.th>
2023-02-07 16:44:57 +0700

    kkkkkkkkk

--- lab16_4.cpp
+++ lab16_4.cpp
@@ -17,3 +17,14 @@ int main(){
 	
 	return 0;
 }
+void shuffle(int &q,int &w,int &e,int &r) {
+	int sf[4]={q,w,e,r} ;
+	for (int i = 0; i < 4 ; i++){
+		int a = rand()%4, b = rand()%4;
+		swap(sf[a],sf[b]) ;
+	}
+	q = sf[0];
+	w = sf[1];
+    e = sf[2];
+	r = sf[3];
+}
